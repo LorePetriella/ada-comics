6618418fff6f0fa360e1813931d590e9761e7643
LorePetriella <98701146+LorePetriella@users.noreply.github.com>
2022-09-18 14:19:52 -0300

    animation

--- app.js
+++ app.js
@@ -13,26 +13,26 @@ getData();
 
 // Animación logo
 
-// element = document.getElementById("animate");
+element = document.getElementById("animate");
 
-// if (element) {
-//   // reset the transition by...
-//   element.addEventListener(
-//     "click",
-//     function (e) {
-//       e.preventDefault;
+if (element) {
+  // reset the transition by...
+  element.addEventListener(
+    "click",
+    function (e) {
+      e.preventDefault;
 
-//       console.log("element", element.classList);
+      console.log("element", element.classList);
 
-//       // removing the class
-//       element.classList.remove("run-animation");
+      // removing the class
+      element.classList.remove("run-animation");
 
-//       // triggering reflow
-//       void element.offsetWidth;
+      // triggering reflow
+      void element.offsetWidth;
 
-//       // and re-adding the class
-//       element.classList.add("run-animation");
-//     },
-//     false
-//   );
-// }
+      // and re-adding the class
+      element.classList.add("run-animation");
+    },
+    false
+  );
+}
