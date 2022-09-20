4d964bb3a073c78946e16b628bda829479f50546
LorePetriella <98701146+LorePetriella@users.noreply.github.com>
2022-09-19 21:00:45 -0300

    trabajando con la presentación

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ getData();
 
 // Animación logo
 
-element = document.getElementById("animate");
+const element = document.getElementById("animate");
+const header = document.querySelector(".header");
+const main = document.querySelector("main");
+const footer = document.getElementById("footer");
 
 if (element) {
   // reset the transition by...
@@ -21,8 +24,6 @@ if (element) {
     "click",
     function (e) {
       e.preventDefault;
-
-      console.log("element", element.classList);
 
       // removing the class
       element.classList.remove("run-animation");
@@ -36,3 +37,19 @@ if (element) {
     false
   );
 }
+
+if (element.classList.contains("run-animation")) {
+  header.classList.add("d-none");
+  main.classList.add("d-none");
+  footer.classList.add("d-none");
+}
+
+const inicio = document.getElementById("enter-btn"); //btn enter
+const presentation = document.getElementById("presentation-container");
+
+inicio.addEventListener("click", () => {
+  presentation.classList.add("d-none");
+  header.classList.remove("d-none");
+  main.classList.remove("d-none");
+  footer.classList.remove("d-none");
+});
